36916dbb9a0823c4c21035d798874ff957be1d5f
TailaiYing <taiying9627@gmail.com>
2025-06-20 11:15:37 -0600

    Adjusted generateInterval so that it returns the starting note and ending note of the interval along with the selected interval

--- src/main/java/com/eartrainer/eartrainer/controller/ExerciseController.java
+++ src/main/java/com/eartrainer/eartrainer/controller/ExerciseController.java
@@ -4,14 +4,17 @@ import com.eartrainer.eartrainer.service.ExerciseService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/exercise")
 public class ExerciseController {
+
     private final ExerciseService exerciseService;
 
     public ExerciseController(ExerciseService exerciseService) {
@@ -19,11 +22,11 @@ public class ExerciseController {
     }
 
     @GetMapping("/interval")
-    public String generateInterval(int level) {
+    public String[] generateInterval(int level) {
         try {
             return exerciseService.generateInterval(level);
         } catch (Exception e) {
-            return e.getMessage();
+            return new String[]{e.getMessage()};
         }
     }
 }
--- src/main/java/com/eartrainer/eartrainer/service/ExerciseService.java
+++ src/main/java/com/eartrainer/eartrainer/service/ExerciseService.java
@@ -1,12 +1,18 @@
 package com.eartrainer.eartrainer.service;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Random;
 import org.springframework.stereotype.Service;
 
 
 @Service
 public class ExerciseService {
+
+    /**
+     * the levels and the corresponding intervals
+     */
     private final Map<Integer, String[]> intervals = Map.of(
             1, new String[]{"M3", "m3"},
             2, new String[]{"M3", "m3", "P5"},
@@ -17,16 +23,77 @@ public class ExerciseService {
             7, new String[]{"M3", "m3", "P5", "P4", "P8", "M6", "m6", "M2", "m2", "M7", "m7"},
             8, new String[]{"M3", "m3", "P5", "P4", "P8", "M6", "m6", "M2", "m2", "M7", "m7", "d5"},
             9, new String[]{"M3", "m3", "P5", "P4", "P8", "M6", "m6", "M2", "m2", "M7", "m7", "d5"},
-            10, new String[]{"M3", "m3", "P5", "P4", "P8", "M6", "m6", "M2", "m2", "M7", "m7", "d5", "M9", "m9"}
+            10, new String[]{"M3", "m3", "P5", "P4", "P8", "M6", "m6", "M2", "m2", "M7", "m7", "d5",
+                    "M9", "m9"}
     );
 
     /**
-     * generates an exercise based on the type and level
-     * info in rcm_requirements.txt
+     * mapping of intervals and half step difference
      */
-    public String generateInterval(int level) {
-            String[] intervalList= intervals.get(level);
-            return intervalList[randomIndex(intervalList.length)];
+    private final Map<String, Integer> intervalMap = new HashMap<>() {{
+        put("m2", 1);
+        put("M2", 2);
+        put("m3", 3);
+        put("M3", 4);
+        put("P4", 5);
+        put("d5", 6);
+        put("P5", 7);
+        put("m6", 8);
+        put("M6", 9);
+        put("m7", 10);
+        put("M7", 11);
+        put("P8", 12);
+        put("m9", 13);
+        put("M9", 14);
+    }};
+
+    /**
+     * all possible starting notes
+     */
+    private final String[] startingNotes = {"C3", "C#3", "D3", "D#3", "E3", "F3", "F#3", "G3",
+            "G#3", "A3",
+            "A#3", "B3", "C4", "C#4", "D4", "D#4", "E4", "F4", "F#4", "G4", "G#4", "A4", "A#4",
+            "B4", "C5"};
+
+    /**
+     * all possible notes
+     */
+    private final String[] allNotes = {"A0", "A#0", "B0", "C1", "C#1", "D1", "D#1", "E1", "F1",
+            "F#1", "G1",
+            "G#1", "A1", "A#1", "B1", "C2", "C#2", "D2", "D#2", "E2", "F2", "F#2", "G2", "G#2",
+            "A2", "A#2", "B2", "C3", "C#3", "D3", "D#3", "E3", "F3", "F#3", "G3", "G#3", "A3",
+            "A#3", "B3", "C4", "C#4", "D4", "D#4", "E4", "F4", "F#4", "G4", "G#4", "A4", "A#4",
+            "B4", "C5", "C#5", "D5", "D#5", "E5", "F5", "F#5", "G5", "G#5", "A5", "A#5", "B5",
+            "C6", "C#6", "D6", "D#6", "E6", "F6", "F#6", "G6", "G#6", "A6", "A#6", "B6", "C7",
+            "C#7", "D7", "D#7", "E7", "F7", "F#7", "G7", "G#7", "A7", "A#7", "B7", "C8"};
+
+    /**
+     * generates an exercise based on the type and level info in rcm_requirements.txt
+     */
+    public String[] generateInterval(int level) {
+        String[] intervalList = intervals.get(level);
+        String selected_interval = intervalList[randomIndex(intervalList.length)];
+
+        /* notes map to keep track of index for easy half step math */
+        HashMap<String, Integer> notes = new HashMap<>();
+        for (int i = 0; i < allNotes.length; i++) {
+            notes.put(allNotes[i], i);
+        }
+
+        /* generate the end note from the start note, randomizing whether to go up or down */
+        String startNote = startingNotes[randomIndex(startingNotes.length)];
+        Integer startingNoteNumber = notes.get(startNote);
+        int halfSteps = intervalMap.get(selected_interval);
+        int upOrDown = new Random().nextBoolean() ? 1 : -1;
+        int endNoteNumber = startingNoteNumber + halfSteps * upOrDown;
+        String endNote = null;
+        for (Map.Entry<String, Integer> entry : notes.entrySet()) {
+            if (entry.getValue() == endNoteNumber) {
+                endNote = entry.getKey();
+            }
+        }
+
+        return new String[]{selected_interval, startNote, endNote};
     }
 
     /**
